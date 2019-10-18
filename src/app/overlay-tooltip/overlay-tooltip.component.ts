380ed470e3aaa61055ed0114af0a51d54394da6b
breakbeatkid <richardpeaston@gmail.com>
2019-10-18 10:55:49 +0100

    1 file modified

--- src/app/overlay-tooltip/overlay-tooltip.component.ts
+++ src/app/overlay-tooltip/overlay-tooltip.component.ts
@@ -11,10 +11,7 @@ import { animate, style, transition, trigger } from '@angular/animations';
       transition(':enter', [
         style({ opacity: 0 }),
         animate(300, style({ opacity: 1 })),
-      ]),
-      transition(':leave', [
-        animate(300, style({ opacity: 0 })),
-      ]),
+      ])
     ]),
   ],
 })
